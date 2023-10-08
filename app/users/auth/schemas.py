e5ffd47b8fdd253fb3f90044eb5818a60a45b329
Artyom Tabolich <feterfox@gmail.com>
2023-10-08 21:45:19 +0300

    login with email instead of username

--- app/users/auth/schemas.py
+++ app/users/auth/schemas.py
@@ -4,3 +4,8 @@ from pydantic import BaseModel
 class Token(BaseModel):
     access_token: str
     refresh_token: str
+
+
+class UserAuthData(BaseModel):
+    email: str
+    password: str
--- app/users/auth/views.py
+++ app/users/auth/views.py
@@ -6,24 +6,22 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from users.auth.services import verify_password, create_access_token, create_refresh_token
-from users.auth.schemas import Token
+from users.auth.schemas import Token, UserAuthData
 from users.models import User as UserModel
 
 router = APIRouter()
 
 
-async def authenticate_user(db: AsyncSession, email: str, password: str):
+async def authenticate_user(db: AsyncSession, email: str, password: str) -> UserModel | Exception:
     user = await UserModel.get_by_email(db, email)
     if not user or not verify_password(password, user.password):
-        return None
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
     return user
 
 
 @router.post("/token", response_model=Token)
-async def login_for_access_token(db: AsyncSession = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()):
-    user = await authenticate_user(db, form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
+async def login_for_access_token(user_data: UserAuthData, db: AsyncSession = Depends(get_db)):
+    user = await authenticate_user(db, user_data.email, user_data.password)
     access_token = create_access_token(data={"sub": user.email})
     refresh_token = create_refresh_token(data={"sub": user.email})
     return {"access_token": access_token, "refresh_token": refresh_token}
