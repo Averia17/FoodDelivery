ee675cdaf150858eb0dc0ce643eca93b4f89c7d0
Artyom Tabolich <feterfox@gmail.com>
2024-01-18 23:20:08 +0300

    add pytest on pre-commit and change test file names

--- app/users/auth/tests.py
+++ app/users/auth/tests.py
@@ -7,12 +7,12 @@ from users.auth.services import get_password_hash
 
 @pytest.mark.asyncio
 async def test_login_for_access_token(client, user_factory):
-    password = 'some_password'
+    password = "some_password"
     hash_password = get_password_hash(password)
     user = await user_factory(password=hash_password)
-    data = {'username': user.email, 'password': password}
+    data = {"username": user.email, "password": password}
 
-    resp = await client.post(url='/api/auth/token', data=data)
+    resp = await client.post(url="/api/auth/token", data=data)
     assert resp.status_code == http.HTTPStatus.OK
-    assert resp.json()['access_token']
-    assert resp.json()['refresh_token']
+    assert resp.json()["access_token"]
+    assert resp.json()["refresh_token"]
